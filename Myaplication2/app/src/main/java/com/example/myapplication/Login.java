9734a2a82f07a5d260d1d683d1e4896d5fa454c7
Jean Pierre <jeanptello.03@gmail.com>
2019-04-02 13:50:27 -0500

    Versión 0.00003

--- Myaplication2/app/src/main/java/com/example/myapplication/Login.java
+++ Myaplication2/app/src/main/java/com/example/myapplication/Login.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
+    EditText v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        v=(EditText)findViewById(R.id.user);
+        v.requestFocus();
     }
 
     protected  void Registrar(View a){
