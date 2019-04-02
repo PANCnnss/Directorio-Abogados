1c5b531fd78181caa73d191f55687fb08eab71d4
Jean Pierre <jeanptello.03@gmail.com>
2019-04-02 13:47:48 -0500

    Versión 0.00003

--- Myaplication2/app/src/main/java/com/example/myapplication/Login.java
+++ Myaplication2/app/src/main/java/com/example/myapplication/Login.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    protected  void Registrar(View a){
+       Intent i = new Intent(this,RegistroPersonas.class);
+       startActivity(i);
     }
 }
