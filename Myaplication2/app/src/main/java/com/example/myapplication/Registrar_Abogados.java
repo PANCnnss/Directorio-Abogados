81bf99b543e6acc7d49a65464e39a9b7a88e29dc
Jean Pierre <jeanptello.03@gmail.com>
2019-04-02 11:49:54 -0500

    Thanks to coaquira

---
+++ Myaplication2/app/src/main/java/com/example/myapplication/Registrar_Abogados.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrar_Abogados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar__abogados);
+    }
+}
