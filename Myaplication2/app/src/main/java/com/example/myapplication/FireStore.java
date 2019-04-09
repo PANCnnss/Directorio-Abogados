b3b868887c0d1ce68621b7597653e7ad964d1e8c
Jean Pierre <jeanptello.03@gmail.com>
2019-04-09 13:20:06 -0500

    Versión 0.00003

--- Myaplication2/app/src/main/java/com/example/myapplication/FireStore.java
+++ Myaplication2/app/src/main/java/com/example/myapplication/FireStore.java
@@ -1,5 +1,36 @@
 package com.example.myapplication;
 
-public class FireStore {
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FireStore {
+    static ProgressDialog pb;
+    public static void registrar(final AppCompatActivity activity, Object obj, String coleccion, String id, ProgressDialog p){
+        pb=p;
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection(coleccion).document(id).set(obj)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(activity,"Rgistrado Correctamente",Toast.LENGTH_LONG).show();
+                        pb.dismiss();
+                    } })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(activity,"Error, por favor intente nuevamente",Toast.LENGTH_LONG).show();
+                pb.dismiss();
+            }
+        });
+
+
+
+    }
 }
