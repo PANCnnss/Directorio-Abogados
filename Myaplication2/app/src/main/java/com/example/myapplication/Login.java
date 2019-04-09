d459e18682ce6a868e465d8ee432136cb065def3
Jean Pierre <jeanptello.03@gmail.com>
2019-04-09 12:03:14 -0500

    Versión 0.00003

--- Myaplication2/app/src/main/java/com/example/myapplication/Login.java
+++ Myaplication2/app/src/main/java/com/example/myapplication/Login.java
@@ -1,11 +1,17 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
@@ -19,12 +25,27 @@ public class Login extends AppCompatActivity {
         v=(EditText)findViewById(R.id.user);
         v.requestFocus();
     }
+    protected  void login(View v){
+        Toast.makeText(this,"ABRIÓ",Toast.LENGTH_LONG).show();
+        Cliente cliente= new Cliente("1","jeanp@gmail.com","123456","Jean","Tello");
 
+        db.collection("Abogados")
+                .add(cliente)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(Login.this,"Registrado",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
     protected  void Registrar(View a){
-
-
-        /*
        Intent i = new Intent(this,RegistroPersonas.class);
-       startActivity(i);*/
+       startActivity(i);
     }
 }
