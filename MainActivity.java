96f87f2e4809d69f8b0df7a3befb4f89f99757c1
VivianIFR <49180072+VivianIFR@users.noreply.github.com>
2019-04-11 00:06:28 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.principal;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,26 +24,39 @@ public class MainActivity extends AppCompatActivity {
     ListView lista;
     String [][] datos={
             {"Juan", "5","Derecho Penal", "Lima"},
-            {"Maria", "4","Derecho administrativo", "Puno"},
+            {"Maria", "4","Derecho civil", "Puno"},
             {"Beatriz", "6","Derecho familiar", "Cusco"},
-            {"Jose", "7","Derecho administrativo", "Arequipa"}
+            {"Jose", "7","Derecho mercantil", "Arequipa"}
     };
+    String[]ciudad={
+            "Amazonas","Áncash","Apurímac", "Arequipa","Ayacucho", "Cajamarca","Cusco",
+            "Huancavelica", "Huánuco","Ica","Junín","La Libertad", "Lambayeque","Lima",
+            "Loreto", "Madre de Dios", "Moquegua", "Pasco", "Piura", "Puno", "San Martín",
+            "Tacna", "Tumbes", "Ucayali"
+    };
+
+    String[] rama={
+            "Derecho administrativo", "Derecho  civil", "Derecho de familia", "Derecho laboral ","Derecho mercantil","Derecho penal"};
     int[] datosImg ={R.drawable.juan, R.drawable.maria, R.drawable.beatriz,R.drawable.jose};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ramas =(Spinner) findViewById(R.id.spramas);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.ramas, android.R.layout.simple_spinner_item);
-        ramas.setAdapter(adapter);
+
+        ArrayAdapter<String>  adapterc=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,ciudad);
+        AutoCompleteTextView textView =(AutoCompleteTextView)
+            findViewById(R.id.ctxtCiudades);
+        textView.setThreshold(3);
+        textView.setAdapter(adapterc);
+
+        ArrayAdapter<String>  adapterr=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,rama);
+        AutoCompleteTextView textView2 =(AutoCompleteTextView)
+                findViewById(R.id.ctxtRamas);
+        textView2.setThreshold(4);
+        textView2.setAdapter(adapterr);
 
         calificacion =(Spinner) findViewById(R.id.spcalificacion);
-        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.calificacion, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.calificacion, R.layout.spinner_item_enchular);
         calificacion.setAdapter(adapter2);
-
-        ciudades =(Spinner) findViewById(R.id.spciudad);
-        ArrayAdapter<CharSequence> adapter3=ArrayAdapter.createFromResource(this,R.array.ciudades, android.R.layout.simple_spinner_item);
-        ciudades.setAdapter(adapter3);
-
 
         lista=(ListView) findViewById(R.id.lvabogados);
         lista.setAdapter(new Adaptador(this,datos, datosImg));
